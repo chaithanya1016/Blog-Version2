bdb61799ba64ae8fe4506ae25544bc69609f9d7e
Chaithanya Reddy <56936221+chaithanya1016@users.noreply.github.com>
2022-09-18 17:34:21 +0530

    Update settings.py

--- FinalProj_Blog/settings.py
+++ FinalProj_Blog/settings.py
@@ -155,6 +155,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'technicalsoul2022@gmail.com'
-EMAIL_HOST_PASSWORD = 'jvrwrljxklnckxiv'
+EMAIL_HOST_USER = 'someone@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 
