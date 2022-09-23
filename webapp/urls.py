8881f17654bce10e7ca304eb5d4ddbebb663ae88
Chaithanya Reddy <56936221+chaithanya1016@users.noreply.github.com>
2022-09-23 22:44:39 +0530

    Update urls.py
    Added like,unlike, counter features

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include, re_path
 from webapp import views as v2
 
+app_name = 'webapps'
+
 urlpatterns = [
     path('addpost/', v2.addarticle, name='addarticle'),
     path('deletearticle/<int:id>', v2.articledelete, name='deletearticle'),
-
+    path('like/', v2.articledetails_view, name='like-article'),
     path('search/', v2.search, name = 'search'),
     path('editpost/<int:id>/', v2.articleupdate, name="articleupdate"),
     path('', v2.articlelist_view, name='article_list'),
