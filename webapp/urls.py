8c42ff68e1f78d80d86de889d9dbc50c74b66e49
Chaithanya Reddy <56936221+chaithanya1016@users.noreply.github.com>
2022-10-03 22:48:59 +0530

    Update urls.py
    app name added

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include, re_path
 from webapp import views as v2
 
-app_name = 'webapps'
+app_name = 'webapp'
 
 urlpatterns = [
     path('addpost/', v2.addarticle, name='addarticle'),
@@ -12,6 +12,8 @@ urlpatterns = [
     path('', v2.articlelist_view, name='article_list'),
     path('<slug:category_slug>/<slug:slug>/', v2.articledetails_view, name='article_detail'),
     path('<slug:slug>/', v2.category_view, name='category_detail'),
+
+
 
 ]
 
